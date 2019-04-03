f913da140dc00c85c43f0958cf75cdc120d72a8d
Aqua-4 <punks23not8dead@gmail.com>
2019-04-03 23:45:17 +0530

    ADD fn()get all heading url

---
+++ psuedo_CODE.md
@@ -0,0 +1,6 @@
+
+1. Generate url for a particular date [viz](https://www.eenadu.net/archivesdet/home/01-04-2019)
+2. all heading url looks like [this](https://www.eenadu.net/archivespage/more/01-04-2019)
+3. get list of all the urls/news headings [li item](https://www.eenadu.net/archivespage/archivenewsdetails/87265/01-04-2019/home)
+   1. section > ul.article-box-list > li > a -> href
+   2. 
--- scapr.py
+++ scapr.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-APP_URL = "https://www.eenadu.net/archivespage/districtnewsdetails/68463/529/n/2019-03-16"
+# APP_URL = "https://www.eenadu.net/archivespage/districtnewsdetails/68463/529/n/2019-03-16"
 
-driver = webdriver.Chrome("./chromedriver")
+driver = webdriver.Chrome("./chromedriver.exe")
 driver.maximize_window()
 
+
+print(headings_url("01-04-2019"))
+
+
+def headings_url(date):
+    #    pass date in dd-mm-yyyy format
+    driver.get("https://www.eenadu.net/archivespage/more/{}".format(date))
+    section = driver.find_element_by_tag_name(
+        "main").find_elements_by_tag_name("section")[1]
+    li_list = section.find_element_by_tag_name(
+        "ul").find_elements_by_tag_name("li")
+    urls = []
+    for i in li_list:
+        urls.append(i.find_element_by_tag_name("a").get_property("href"))
+    return urls
